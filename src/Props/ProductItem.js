9010fa241a15740c77c4fbcd024e965b2de9b31c
dnh2703 <dnh2703@gmail.com>
2022-06-07 16:37:31 +0700

    bai3

--- src/Props/ProductItem.js
+++ src/Props/ProductItem.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ProductItem extends Component {
+
+
+
   render() {
     return (
-      <div>ProductItem</div>
+      <div style={{ style: '300px' }} className="card text-left">
+        <img className="card-img-top" src={this.props.dataProductItem.image} alt={this.props.dataProductItem.image} />
+        <div className="card-body">
+          <h4 className="card-title">{this.props.dataProductItem.name}</h4>
+          <p className="card-text">{this.props.dataProductItem.price}</p>
+          <button className='text-white w3-button w3-black'>View detail</button>
+
+        </div>
+      </div>
+
     )
   }
 }
--- src/Props/ProductList.js
+++ src/Props/ProductList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import ProductItem from './ProductItem'
 
 export default class ProductList extends Component {
+
+
+    renderProductItem = () => {
+        return this.props.arrProduct.map((item, index) => {
+            return (
+                <div className='col-4 mt-3 w3-animate-zoom' key={index}>
+                    <ProductItem dataProductItem={item} />
+                </div>
+
+            )
+        })
+    }
+
     render() {
 
         console.log(this.props.arrProduct)
         return (
-            <div>ProductList</div>
+            <div className='row'>
+                {this.renderProductItem()}
+            </div>
         )
     }
 }
