b3f60fe085ad35f1c8c84fed1c325a60adabef01
dnh2703 <dnh2703@gmail.com>
2022-06-08 09:06:02 +0700

    bai5

--- src/App.jsx
+++ src/App.jsx
@@ -2,68 +2,72 @@ import React, { Component } from "react";
 import DemoProps from "./Props/DemoProps";
 import ProductList from "./Props/ProductList";
 import dataJson from "./Data/data.json";
+import DanhSachSanPham from "./Props/DanhSachSanPham";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        {/* <DemoProps></DemoProps> */}
-        <div className="row">
-          <div className="col-4">
-            <div
-              className="nav flex-column nav-pills justify-content-center"
-              style={{ minHeight: "500px" }}
-              id="v-pills-tab"
-              role="tablist"
-              aria-orientation="vertical"
-            >
-              <a
-                className="nav-link active"
-                id="v-pills-home-tab"
-                data-toggle="pill"
-                href="#v-pills-home"
-                role="tab"
-                aria-controls="v-pills-home"
-                aria-selected="true"
-              >
-                Home
-              </a>
-              <a
-                className="nav-link"
-                id="v-pills-profile-tab"
-                data-toggle="pill"
-                href="#v-pills-profile"
-                role="tab"
-                aria-controls="v-pills-profile"
-                aria-selected="false"
-              >
-                Shop
-              </a>
-            </div>
-          </div>
-          <div className="productList col-8">
-            <div className="tab-content" id="v-pills-tabContent">
-              <div
-                className="tab-pane fade show active"
-                id="v-pills-home"
-                role="tabpanel"
-                aria-labelledby="v-pills-home-tab"
-              >
-                {/* <ProductList></ProductList> */}
-                <ProductList arrProduct={dataJson}></ProductList>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-profile"
-                role="tabpanel"
-                aria-labelledby="v-pills-profile-tab"
-              >
-                Shop
-              </div>
-            </div>
-          </div>
-        </div>
+      <div>
+        <DanhSachSanPham></DanhSachSanPham>
       </div>
+      // <div className="container-fluid">
+      //   {/* <DemoProps></DemoProps> */}
+      //   <div className="row">
+      //     <div className="col-4">
+      //       <div
+      //         className="nav flex-column nav-pills justify-content-center"
+      //         style={{ minHeight: "500px" }}
+      //         id="v-pills-tab"
+      //         role="tablist"
+      //         aria-orientation="vertical"
+      //       >
+      //         <a
+      //           className="nav-link active"
+      //           id="v-pills-home-tab"
+      //           data-toggle="pill"
+      //           href="#v-pills-home"
+      //           role="tab"
+      //           aria-controls="v-pills-home"
+      //           aria-selected="true"
+      //         >
+      //           Home
+      //         </a>
+      //         <a
+      //           className="nav-link"
+      //           id="v-pills-profile-tab"
+      //           data-toggle="pill"
+      //           href="#v-pills-profile"
+      //           role="tab"
+      //           aria-controls="v-pills-profile"
+      //           aria-selected="false"
+      //         >
+      //           Shop
+      //         </a>
+      //       </div>
+      //     </div>
+      //     <div className="productList col-8">
+      //       <div className="tab-content" id="v-pills-tabContent">
+      //         <div
+      //           className="tab-pane fade show active"
+      //           id="v-pills-home"
+      //           role="tabpanel"
+      //           aria-labelledby="v-pills-home-tab"
+      //         >
+      //           {/* <ProductList></ProductList> */}
+      //           <ProductList arrProduct={dataJson}></ProductList>
+      //         </div>
+      //         <div
+      //           className="tab-pane fade"
+      //           id="v-pills-profile"
+      //           role="tabpanel"
+      //           aria-labelledby="v-pills-profile-tab"
+      //         >
+      //           Shop
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
 }
