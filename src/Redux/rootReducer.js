ce1834fb151fd39e6dbfba76db29d3515f80dfeb
dnh2703 <dnh2703@gmail.com>
2022-06-16 20:42:02 +0700

    bai14

---
+++ src/Redux/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+
+const rootReducer = combineReducers({ //Store tong cua ung dung
+
+
+})
+
+export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// step redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './Redux/rootReducer';
+
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode store={store}>
     <App />
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
