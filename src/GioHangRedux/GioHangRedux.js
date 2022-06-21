bc508b90a5e81d66dba565124b76378051676047
dnh2703 <dnh2703@gmail.com>
2022-06-21 10:54:10 +0700

    bai18

--- src/GioHangRedux/GioHangRedux.js
+++ src/GioHangRedux/GioHangRedux.js
@@ -38,23 +38,38 @@ class GioHangRedux extends Component {
                                                     <td><img src={spGH.hinhAnh} alt={spGH.hinhAnh} width={50} height={50} /></td>
                                                     <td>{spGH.tenSP}</td>
                                                     <td>{spGH.donGia.toLocaleString()}</td>
-                                                    <td>{spGH.soLuong}</td>
+                                                    <td>
+                                                        <button className='btn btn-success' onClick={() => { this.props.tangGiamSoLuong(spGH.maSP, true) }}>+</button>
+                                                        {spGH.soLuong}
+                                                        <button className='btn btn-success' onClick={() => { this.props.tangGiamSoLuong(spGH.maSP, false) }}>-</button>
+                                                    </td>
                                                     <td>{(spGH.soLuong * spGH.donGia).toLocaleString()}</td>
+                                                    <td><button className='btn btn-danger' onClick={() => {
+                                                        this.props.xoaGioHang(spGH.maSP)
+                                                    }}
+                                                    >Xoa</button></td>
+
                                                 </tr>
                                             })}
                                         </tbody>
+                                        <tfoot>
+                                            <th colSpan={5}></th>
+                                            <th>Tong tien</th>
+                                            <th>{this.props.gioHang.reduce((tongTien, spGioHang, index) => {
+                                                return tongTien += spGioHang.soLuong * spGioHang.donGia;
+                                            }, 0).toLocaleString()}</th>
+                                        </tfoot>
                                     </table>
                                 </div>
                                 <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" className="btn btn-primary">Save</button>
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Dong</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-            </div>
+            </div >
         )
     }
 }
@@ -68,4 +83,29 @@ const mapStateToProps = state => { // state la state tong chua cac ung dung cua 
 
 }
 
-export default connect(mapStateToProps)(GioHangRedux)
+// Ham dua du lieu len reducer
+const mapDispatchToProps = (dispatch) => {
+    return {
+        xoaGioHang: (maSP) => {
+            let action = {
+                type: 'XOA_GIO_HANG',
+                maSP
+            }
+            //Dung phuong thuc dispatch redux cung cap de dua du lieu len reducer
+            console.log(maSP)
+            dispatch(action)
+        },
+        tangGiamSoLuong: (maSP, tangGiam) => { // tangGiam - true => Xu ly tang, tangGiam = false => Xu ly giam
+            // tao action de dua du lieu len reducer
+            let action = {
+                type: 'TANG_GIAM_SO_LUONG',// Thuoc tinh bat buoc de biet chay vao case nao trong tat ca reducer
+                maSP,
+                tangGiam
+            }
+            // Dua action len reducer moi lan nguoi dung click vao
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GioHangRedux)
--- src/Redux/BaiTapGioHangReducer.js
+++ src/Redux/BaiTapGioHangReducer.js
@@ -4,14 +4,13 @@
 import { type } from "@testing-library/user-event/dist/type";
 
 const stateGioHang = {
-    gioHang: [{ maSP: 1, tenSP: 'sp mac dinh', hinhAnh: '', soLuong: 1, donGia: 1000 }]
+    gioHang: []
 }
 
 
 
 const BaiTapGioHangRedux = (state = stateGioHang, action) => {
 
-    // eslint-disable-next-line default-case
     switch (action.type) {
         case 'THEM_GIO_HANG': {
             let index = state.gioHang.findIndex(spGH => spGH.maSP === action.spGioHang.maSP);
@@ -22,17 +21,48 @@ const BaiTapGioHangRedux = (state = stateGioHang, action) => {
                 state.gioHang.push(action.spGioHang)
             }
             // SetState
-
+            // Cap nhat lai state.gioHang
             state.gioHang = [...state.gioHang]
 
-            console.log('gioHang', state.gioHang)
+            // console.log('gioHang', state.gioHang)
 
-            return { ...state};
+            return { ...state };
+        }; break;
 
-        }
+        case 'XOA_GIO_HANG': {
+            let gioHangCapNhat = [...state.gioHang]
+            // Tim ra phan tu can xoa dua vao maSP
+            let index = gioHangCapNhat.findIndex(spGH => spGH.maSP === action.maSP);
+            if (index !== -1) {
+                gioHangCapNhat.splice(index, 1);
+            }
+            // Cap nhat lai state gio hang moi de component render lai
+            state.gioHang = gioHangCapNhat;
+            return { ...state }
+        }; break;
+        case 'TANG_GIAM_SO_LUONG': {
+            let gioHangCapNhat = [...state.gioHang]
+            // Xu ly tang giam tren gio hang cap nhat
+            let index = gioHangCapNhat.findIndex(spGH => spGH.maSP === action.maSP)
+            if (index !== -1) {
+                if (action.tangGiam) {
+                    gioHangCapNhat[index].soLuong += 1;
+                } else {
+                    if (gioHangCapNhat[index].soLuong > 1) {
+                        gioHangCapNhat[index].soLuong -= 1;
+                    } else {
+                        alert('So luong toi thieu la 1')
+                    }
+                }
+            }
+            // Lay gia tri gio hang cap nhat gan vao state.gioHang
+            state.gioHang = gioHangCapNhat;
+            return { ...state }
+        }; break;
+        default: return { ...state }
+
     }
 
-    return state;
 }
 
 export default BaiTapGioHangRedux;
